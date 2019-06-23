573a1ccd0996cd77e2eba7dd7f0dabf20f42c344
bezikan <myphilosophy25@gmail.com>
2019-06-23 02:18:57 +0200

    h1 to h3

--- pythonapp/views.py
+++ pythonapp/views.py
@@ -10,5 +10,11 @@ def eggs(request):
 def count(request):
     fulltext = request.GET['fulltext']
     wordlist = fulltext.split()
+    worddict
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
     print(fulltext)
     return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist)})
